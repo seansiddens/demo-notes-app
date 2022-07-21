40527bae1a1d484ffa832cb687f6d785fdfc1754
Sean Siddens <seansiddens@gmail.com>
2022-07-20 21:12:00 -0700

    Enabling CORS

--- services/util/handler.js
+++ services/util/handler.js
@@ -15,7 +15,11 @@ export default function handler(lambda) {
 
         return {
             statusCode,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true,
+            },
             body: JSON.stringify(body),
-        }
+        };
     }
 }
