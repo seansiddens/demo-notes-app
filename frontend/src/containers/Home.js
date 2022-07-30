4b20562d1bc9c353cc002feac0b9321b28102f93
Sean Siddens <seansiddens@gmail.com>
2022-07-29 20:02:30 -0700

    Listing notes

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { API } from "aws-amplify";
 import ListGroup from "react-bootstrap/ListGroup";
 import { useAppContext } from "../lib/contextLib";
 import { onError } from "../lib/errorLib";
+import { BsPencilSquare } from "react-icons/bs";
+import { LinkContainer } from "react-router-bootstrap";
+
 import "./Home.css";
 
 export default function Home() {
@@ -9,8 +13,54 @@ export default function Home() {
     const { isAuthenticated } = useAppContext();
     const [isLoading, setIsLoading] = useState(true);
 
+    useEffect(() => {
+        async function onLoad() {
+            if (!isAuthenticated) {
+                return;
+            }
+
+            try {
+                const notes = await loadNotes();
+                setNotes(notes);
+            } catch (e) {
+                onError(e);
+            }
+
+            setIsLoading(false);
+        }
+
+        onLoad();
+    }, [isAuthenticated]); // Will call the function every time 'isAuthenticated' changes.
+
+    function loadNotes() {
+        return API.get("notes", "/notes");
+    }
+
+
     function renderNotesList(notes) {
-        return null;
+        return (
+            <>
+                <LinkContainer to="/notes/new">
+                    <ListGroup.Item action className="py-3 text-nowrap text-truncate">
+                        <BsPencilSquare size={17} />
+                        <span className="ml-2 font-weight-bold">Create a new note</span>
+                    </ListGroup.Item>
+                </LinkContainer>
+                {notes.map(({ noteId, content, createdAt }) => (
+                    <LinkContainer key={noteId} to={`/notes/${noteId}`}>
+                        <ListGroup.Item action>
+                            <span className="font-weight-bold">
+                                {content.trim().split("\n")[0]}
+                            </span>
+                            <br />
+                            <span className="text-muted">
+                                Created: {new Date(createdAt).toLocaleString()}
+                            </span>
+                        </ListGroup.Item>
+                    </LinkContainer>
+                ))}
+            </>
+        );
     }
 
     function renderLander() {
